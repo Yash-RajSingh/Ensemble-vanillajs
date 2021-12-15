932fe696a5e3246950d1b8b933feda4921b0a359
Yash-RajSingh <yashraj45202@gmail.com>
2021-12-16 02:29:32 +0530

    Added Login/Signup

--- scripts/login.js
+++ scripts/login.js
@@ -3,7 +3,6 @@ function toggle() {
     var lcard = document.getElementById("card-login");
     var scard = document.getElementById("card-signup");
     var head = document.querySelector('h1');
-    console.log(head);
     if (scard.style.display === "none") {
         lcard.style.display = "none";
         scard.style.display = "block";
@@ -18,13 +17,25 @@ function toggle() {
 
 const signup = document.getElementById('signup');
 signup.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    const email = document.getElementById('semail').value;
-    const sname = document.getElementById('susername').value;
-    const password = document.getElementById('spassword').value;
-    const question = document.getElementById('question').value;
-    const answer = document.getElementById('answer').value;
-    console.log(email, sname,password, question, answer);
+  e.preventDefault();
+  const email = document.getElementById('semail').value;
+  const sname = document.getElementById('susername').value;
+  const password = document.getElementById('spassword').value;
+  const question = document.getElementById('question').value;
+  const answer = document.getElementById('answer').value;
+  console.log(email, sname,password, question, answer);
+  const base = `https://61c0-146-196-33-36.ngrok.io/authentication/register`;
+  const req = `?username=${sname}&email=${email}&password=${password}`;
+  const query = `${base}${req}`;
+  console.log(query);
+  const Sign = async () => {
+    const response = await fetch(query, {
+      method: 'POST'
+  });
+  const data = await response.json();
+  console.log(data);
+  } 
+  Sign();
 })
 
 
@@ -34,5 +45,20 @@ login.addEventListener('submit', (e) =>{
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    console.log(email, password);
+    const base = `https://61c0-146-196-33-36.ngrok.io/authentication/login`;
+    const req = `?email=${email}&password=${password}`;
+    const query = `${base}${req}`;
+    const logy = async () => {
+      const response = await fetch(query, {
+        method: 'POST'
+    });
+    const data = await response.json();
+    console.log(data.message);
+
+      if(data.status == 200){
+        window.location.href = '../boards.html';
+      }
+    } 
+    logy();
+
 })
